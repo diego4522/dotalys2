b5da901911e23494fb23a03f4241ff70589fc13a
t.mahlmann <t.mahlmann@gmail.com>
2014-05-26 19:49:35 +0000

    code cleanup

--- Dotalys2/src/de/lighti/parsing/HeroTracker.java
+++ Dotalys2/src/de/lighti/parsing/HeroTracker.java
@@ -10,8 +10,6 @@ import de.lighti.model.game.Hero;
 public class HeroTracker extends DefaultGameEventListener {
     private final AppState state;
 
-    private final int o = 0;
-
     public HeroTracker( AppState state ) {
         super();
         this.state = state;
@@ -21,23 +19,20 @@ public class HeroTracker extends DefaultGameEventListener {
     @Override
     public void entityCreated( long tickMs, Entity e ) {
         if (e.getEntityClass().getName().contains( "CDOTA_Unit_Hero_" )) {
-//          System.out.println( e.getId() + " " + e.getEntityClass().getName() );
+
             if (state.getHero( e.getId() ) == null) {
                 state.setHero( e.getId(), new Hero( e.getEntityClass().getName() ) );
             }
             for (final Property p : e.getProperties()) {
                 final String name = p.getName();
-                if (e.getEntityClass().getName().contains( "Ursa" ) && name.contains( "m_hItems" )) {
+                if (name.contains( "m_hAbilities" )) {
 
-                }
-                else if (name.contains( "m_hAbilities" )) {
-//                    System.out.println( e.getEntityClass().getDtName() );
                     int value = (int) e.getProperty( name ).getValue();
                     if (value != 0x1FFFFF) {
                         final Hero h = state.getHero( e.getId() );
                         final int slot = Integer.parseInt( name.substring( name.lastIndexOf( "." ) + 1 ) );
                         value &= 0x7ff;
-//                    System.out.println( e.getEntityClass().getDtName() + e.getId() + " " + slot + "->" + value );
+
                         h.addAbility( DotaPlay.getTickMs(), slot, value );
                     }
                 }
@@ -73,7 +68,6 @@ public class HeroTracker extends DefaultGameEventListener {
 
                     final int slot = Integer.parseInt( name.substring( name.lastIndexOf( "." ) + 1 ) );
                     value &= 0x7ff;
-//                System.out.println( e.getEntityClass().getDtName() + e.getId() + " " + slot + "->" + value );
                     h.addAbility( DotaPlay.getTickMs(), slot, value );
                 }
             }
@@ -85,9 +79,6 @@ public class HeroTracker extends DefaultGameEventListener {
                     h.addDeath( DotaPlay.getTickMs(), x, y );
                 }
             }
-//            else if (e.getEntityClass().getDtName().contains( "Ursa" )) {
-//                System.out.println( e.getId() + "." + name + " " + oldValue + "->" + e.getProperty( name ).getValue() );
-//            }
         }
     }
 
