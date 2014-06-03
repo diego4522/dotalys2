04ca1f1fc91379ff32f097f5e4464f0f13eb9c16
t.mahlmann@gmail.com <t.mahlmann@gmail.com@0ffc5698-df4b-45d0-dc68-d795b40ba0b9>
2014-06-03 11:56:00 +0000

    replaced trillions of labels with a table moved constants over to statics

--- trunk/Dotalys2/src/de/lighti/model/Statics.java
+++ trunk/Dotalys2/src/de/lighti/model/Statics.java
@@ -5,4 +5,13 @@ public interface Statics {
     String SECONDS = "Seconds";
     String RADIANT = "Radiant";
     String DIRE = "Dire";
+    String NAME = "Name";
+    String TEAM = "Team";
+    String HERO = "Hero";
+    String TOTAL_GOLD = "Total Gold";
+    String GOLD_PER_MINUTE = "Gold per Minute";
+    String TOTAL_XP = "Total XP";
+    String XP_PER_MINUTE = "XP per Minute";
+    String UNKNOWN_HERO = "<unknown>";
+    String PLAYER = "Player";
 }
