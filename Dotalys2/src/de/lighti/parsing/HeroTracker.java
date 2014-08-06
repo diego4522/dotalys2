d0db8f5469fa28c7024b8ba95fdb2ef13baf103b
t.mahlmann <t.mahlmann@gmail.com>
2014-08-06 16:13:06 +0000

    Store the real DotaItem reference in Hero

--- Dotalys2/src/de/lighti/parsing/HeroTracker.java
+++ Dotalys2/src/de/lighti/parsing/HeroTracker.java
@@ -1,5 +1,10 @@
 package de.lighti.parsing;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 import java.util.logging.Logger;
 
 import de.lighti.DefaultGameEventListener;
@@ -9,14 +14,17 @@ import de.lighti.model.Entity;
 import de.lighti.model.Property;
 import de.lighti.model.game.Ability;
 import de.lighti.model.game.Hero;
+import de.lighti.model.state.ParseState;
 
 public class HeroTracker extends DefaultGameEventListener {
     private final AppState state;
 
+    private final Map<Hero, TreeMap<Long, Integer[]>> itemCache;
+
     public HeroTracker( AppState state ) {
         super();
         this.state = state;
-
+        itemCache = new HashMap<Hero, TreeMap<Long, Integer[]>>();
     }
 
     @Override
@@ -24,7 +32,7 @@ public class HeroTracker extends DefaultGameEventListener {
         if (e.getEntityClass().getName().contains( "CDOTA_Unit_Hero_" )) {
 
             if (state.getHero( e.getId() ) == null) {
-                state.setHero( e.getId(), new Hero( state.getHeroName( e.getEntityClass().getName() ) ) );
+                state.setHero( e.getId(), new Hero( AppState.getHeroName( e.getEntityClass().getName() ) ) );
             }
             for (final Property p : e.getProperties()) {
                 final String name = p.getName();
@@ -66,10 +74,12 @@ public class HeroTracker extends DefaultGameEventListener {
                 int value = (int) e.getProperty( name ).getValue();
                 if (value != 0x1FFFFF) {
                     value &= 0x7ff;
-                    h.setItem( DotaPlay.getTickMs(), slot, value );
+//                    h.setItem( DotaPlay.getTickMs(), slot, value );
+                    setItemInCache( h, DotaPlay.getTickMs(), slot, value );
                 }
                 else {
-                    h.setItem( DotaPlay.getTickMs(), slot, null );
+//                    h.setItem( DotaPlay.getTickMs(), slot, null );
+                    setItemInCache( h, DotaPlay.getTickMs(), slot, null );
                 }
             }
             else if (name.contains( "m_hAbilities" )) {
@@ -95,6 +105,51 @@ public class HeroTracker extends DefaultGameEventListener {
                     h.addDeath( DotaPlay.getTickMs(), x, y );
                 }
             }
+        }
+    }
+
+    @Override
+    public void parseComplete( long tickMs, ParseState state ) {
+        super.parseComplete( tickMs, state );
+
+        for (final Hero h : itemCache.keySet()) {
+            final TreeMap<Long, Integer[]> heroItems = itemCache.get( h );
+            for (final Long l : heroItems.keySet()) {
+                final Integer[] frameItems = heroItems.get( l );
+                for (int slot = 0; slot < frameItems.length; slot++) {
+                    if (frameItems[slot] != null) {
+                        h.setItem( l, slot, this.state.getItem( l, frameItems[slot] ) );
+                    }
+                    else {
+                        h.setItem( l, slot, null );
+                    }
+                }
+            }
+        }
+
+        itemCache.clear();
+    }
+
+    private void setItemInCache( Hero h, long tickMs, int slot, Integer value ) {
+        TreeMap<Long, Integer[]> items = itemCache.get( h );
+        if (items == null) {
+            items = new TreeMap<Long, Integer[]>();
+            items.put( 0l, new Integer[Hero.BAG_SIZE] );
+            itemCache.put( h, items );
+        }
+        if (items.containsKey( tickMs )) {
+            //Just store the update
+            items.get( tickMs )[slot] = value;
+        }
+        else {
+            //We advanced. Push the current bag configuration, calculate the diff to the previous one and make
+            //a new array for the new tick
+            final Entry<Long, Integer[]> current = items.floorEntry( tickMs );
+
+            final Integer[] newBag = Arrays.copyOf( current.getValue(), current.getValue().length );
+            newBag[slot] = value;
+            items.put( tickMs, newBag );
+
         }
     }
 
